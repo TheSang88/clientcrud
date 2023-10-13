ba7fffd518bc85d1fe8315071c60169015e85c3a
TheSang88 <phamthesang88@gmail.com>
2023-10-13 10:23:35 +0700

    add spinner

---
+++ src/component/Spinner.js
@@ -0,0 +1,12 @@
+import Spinner from 'react-bootstrap/Spinner';
+
+const VariantsExample = () => {
+    return (
+        <div>
+            <h3>Please wait for the server</h3>
+            <Spinner animation="border" variant="primary" />
+        </div>
+    );
+}
+
+export default VariantsExample;
--- src/component/lists.js
+++ src/component/lists.js
@@ -2,17 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
+import VariantsSpinner from './Spinner.js'
 function User() {
 
     const [users, setUser] = useState([{
         Name: "sang", Email: "sang@gmail.com", Age: "20"
     }]);
 
+    const [loading, setLoading] = useState(false);
+
+
     useEffect(() => {
+        setLoading(true);
         axios.get('https://servercrud.onrender.com/User/listuser')
-            .then(result => setUser(result.data))
-            .catch(err => console.log(err))
+            .then(result => {
+                setLoading(false);
+                setUser(result.data)
+            })
+            .catch(err => {
+                setLoading(true);
+                console.log(err)
+            })
     }, [])
+
+
+
+
 
     const handleDelete = (id) => {
         axios.delete('https://servercrud.onrender.com/User/deleteUser/' + id)
@@ -23,8 +38,18 @@ function User() {
 
             .catch(errr => console.log(errr))
     }
+
+
+
+
+
+
+
+
     return (
         <div>
+
+
             <h3 className="form-group" align="center">Persons List</h3>
             <Link to='/create' className='btn btn-success form-group'>ADD +</Link>
             <Table striped bordered hover className="table table-striped form-group" style={{ marginTop: 20 }}>
@@ -37,28 +62,27 @@ function User() {
                         <th colSpan="2">Action</th>
                     </tr>
                 </thead>
-
-
-                <tbody>
-                    {
-                        users.map((user) => {
-                            return (
-                                <tr>
-                                    <td>{user.name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.age}</td>
-                                    <td>
-                                        <Link to={'/edit/' + user._id} className='btn btn-success'>update</Link>
-                                        <button className='btn btn-danger'
-                                            onClick={(e) => handleDelete(user._id)}
-                                        >Delete</button>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-
+                {loading ? (<VariantsSpinner />) : (
+                    <tbody>
+                        {
+                            users.map((user) => {
+                                return (
+                                    <tr>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.age}</td>
+                                        <td>
+                                            <Link to={'/edit/' + user._id} className='btn btn-success'>update</Link>
+                                            <button className='btn btn-danger'
+                                                onClick={(e) => handleDelete(user._id)}
+                                            >Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                )}
             </Table>
 
 
